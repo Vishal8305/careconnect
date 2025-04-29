e33d05747e928693b0f32f8524f06f108dd1145e
Vishal8305 <vraut8305@gmail.com>
2025-04-29 17:09:01 +0530

    profile avtar fix

--- src/components/common/UserProfile.jsx
+++ src/components/common/UserProfile.jsx
@@ -14,6 +14,7 @@ import {
   CircularProgress,
   InputLabel,
   Stack,
+  CardMedia,
 } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -137,10 +138,11 @@ const UserProfile = () => {
                 </Typography>
               </Box>
             ) : (
-              <Avatar
+              <CardMedia
+              component="img"
                 src={profileData?.selectedImage}
                 alt={profileData?.firstName}
-                sx={{ width: 144, height: 144, borderRadius: 2 }}
+                sx={{  bgcolor: "#eaefff",width: 144, height: 144, borderRadius: 2, objectFit:'contain' }}
               />
             )}
           </Box>
